8089e4829e3ae4ca1f37bb9e24cb93257d189467
Анатолий <Chepanov199287@gmail.com>
2023-03-28 20:40:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_4_4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string Type, double Age, int NameCount) Pet;
+
+            Console.WriteLine("Введите имя питомца");
+            Pet.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите тип питомца: собака, рыбка, хомячок");
+            Pet.Type = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст питомца");
+            Pet.Age = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите длину имени питомца");
+            Pet.NameCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
